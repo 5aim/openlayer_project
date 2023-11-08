346446c63e71a06018d661cd2016a6dfa6019357
5aim <hyukjin720@gmail.com>
2023-11-09 03:45:59 +0900

    Navigation Elements Logic

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function init() {
 		return Styles
 	}
 
-	const StyleForSelect = function(feature){
+	const styleForSelect = function(feature){
 		let nodeId = feature.get('node_id');
 		let nodeIdString = nodeId.toString();
 		let placename = feature.get('placename');
@@ -117,20 +117,52 @@ function init() {
 		let currentActiveStyledElement = document.querySelector('.active');
 		currentActiveStyledElement.className = currentActiveStyledElement.className.replace('active', '');
 		clickedAnchorElement.className = 'active';
-
-		// change the view based on the feature
-		let featureCoordinates = feature.get('geometry').getCoordinates();
-		mapView.animate({center: featureCoordinates}, {zoom: 17})
 		
+		// Default style for all features
 		let aussieCitiesFeatures = cheonanCitiesLayer.getSource().getFeatures();
 		aussieCitiesFeatures.forEach(function(feature){
 			feature.setStyle(cheonanCitiesStyle);
 		})
-		feature.setStyle(StyleForSelect)
 
-		let featureName = feature.get('cityname');
-		let featureImage = feature.get('cityimage');
-		cityNameElement.innerHTML = 'Name is : ' + featureName
-		cityImageElement.setAttribute('src', './data/image/' + featureImage + '.jpg')
+		// Home Element : change content in the menu to home
+		if(clickedAnchorElement.id === 'Home'){
+			mapView.animate({center : CHEONAN}, {zoom:13.5})
+			cityNameElement.innerHTML = 'Welcome to cheonan cities';
+			cityImageElement.setAttribute('src', './data/image/Home.jpg');
+		}
+		
+		// Change view, and content in the menu based on the feature
+		else {
+			feature.setStyle(styleForSelect)
+			let featureCoordinates = feature.get('geometry').getCoordinates();
+			mapView.animate({center: featureCoordinates}, {zoom: 17})
+			
+			let featureName = feature.get('cityname');
+			let featureImage = feature.get('cityimage');
+			cityNameElement.innerHTML = 'Name is : ' + featureName
+			cityImageElement.setAttribute('src', './data/image/' + featureImage + '.jpg')
+		}
+	}
+
+	// Navigation Button Logic
+	const anchorNavElements = document.querySelectorAll('.column-navigation > a')
+	for(let anchorNavElement of anchorNavElements){
+		anchorNavElement.addEventListener('click', function(e){
+			let clickedAnchorElement = e.currentTarget;
+			let clickedAbchorElementID = clickedAnchorElement.id;
+			let cheonanCitiesFeatures = cheonanCitiesLayer.getSource().getFeatures();
+			
+			cheonanCitiesFeatures.forEach(function(feature){
+				let featureCityName = feature.get('cityname');
+				if(clickedAbchorElementID === featureCityName){
+					mainLogic(feature, clickedAnchorElement);
+				}
+			})
+
+			// Home Navigation Case
+			if(clickedAbchorElementID === 'Home'){
+				mainLogic(undefined, clickedAnchorElement);
+			}
+		})
 	}
 }
